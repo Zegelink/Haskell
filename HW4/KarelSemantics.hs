0f1d4904788d9f75f4c20b3fcafa966380539334
Zegelink <supercomboooo@gmail.com>
2017-02-25 21:44:30 -0800

    finish test

--- HW4/KarelSemantics.hs
+++ HW4/KarelSemantics.hs
@@ -5,12 +5,18 @@ import Data.Function (fix)
 
 import KarelSyntax
 import KarelState
+import KarelExamples
 
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
 test (Not t) w r = not (test t w r)
-test (Facing c) w r = if (c == (getFacing r)) then Ture else False
+test (Facing c) _ r = if (c == (getFacing r)) then True else False
+-- neighbor (cardTurn d (getFacing r)) (getPos r) 
+-- will get the new position
+test (Clear d) w r = isClear (neighbor (cardTurn d (getFacing r)) (getPos r) ) w
+test Beeper w r = hasBeeper (getPos r) w
+test Empty _ r = if (getBag r) > 0 then False else True
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
